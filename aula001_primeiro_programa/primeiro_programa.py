f964612dbd0c7a583fff4943d45642b0d6e0a933
daniloamarojf <daniloramarojg@gmail.com>
2024-04-12 21:21:53 -0300

    Hello World

---
+++ aula001_primeiro_programa/primeiro_programa.py
@@ -0,0 +1,19 @@
+# Curso de Desenvolvimento de Sistemas
+# Turma 0152
+# Autor: Danilo Rogerio Magalhaes Amaro
+# Data: 12/04/2024
+# Primeiro Programa em Python
+
+# Importando as bibliotecas
+import os
+
+
+# limpando o terminal
+os.system('cls')
+
+# O comando print() execulta uma saida via terminal
+
+print('-' * 70)
+print('Hello World!!!')
+print('-' * 70)
+print()
