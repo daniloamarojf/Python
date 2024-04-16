5b3710ee07547fe7dde531edf439eb69659f295f
daniloamarojf <daniloramarojg@gmail.com>
2024-04-15 21:39:47 -0300

    variaveis

---
+++ aula002_variaveis/estudo_variaveis.py
@@ -0,0 +1,27 @@
+# importando a biblioteca do sistema
+import os
+
+
+# Limpando o terminal
+os.system('cls')
+
+print('-' *70)  # firula
+print('Estudo de Variáveis')
+print('=' *70)  # firula
+
+# As variaveis são declaradas por inferência
+nome = 'Jhon Doe'
+nascimento = 1970
+altura = 1.80
+peso = 75.5
+doador = true
+complexo = 3j # Python trabalha diretamente com números complexos
+PI = 3.14 #isso pe uma constante, seu valor não deve ser alterado
+
+# saida utilizando o metodo type() para verificar o
+# tipo de variáveis
+print('\033[0m \033[31mTipos declarados:\033[0m)]] 
+print('\033[0m A var \033[32m nome \033[0m e do tipo: ', type (nome)) 
+print('\033[0m A var \033[32m nascimento \033[0m e do tipo: ', typo(nascimento)]]                                          
+print('\033[0m A var \033[32m altura \033[0m e do tipo:]]
+print('\033[0m A var \033 
