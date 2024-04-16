ad5d8b42aad63348c3e38a93c39a2e5a5d10af60
daniloamarojf <daniloramarojg@gmail.com>
2024-04-15 21:00:28 -0300

    dados pessoias

---
+++ testes/teste.py
@@ -0,0 +1,22 @@
+# Testando os comandos do python
+# Dia: 15/04/2024
+
+print('*' * 70 )
+print('               Danilo Rogerio Magalhães Amaro              ')
+print('*' * 70)
+
+# Importando biblioteca
+import os
+
+
+# Limpar terminal
+os.system('cls')
+
+# Dados pessoais
+
+print('Endereço: Rua Olimpio Costa, 145 - casa 2 - Benfica-JF')
+print('CPF: 044.899.726-60')
+print('Mãe: Maria da Aparecida Magalhães Amaro')
+print('Pai: Rogerio Amaro')
+
+print('')
