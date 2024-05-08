7a43b4e36df566c53208d0f0f5d3a77002c535ae
daniloamarojf <daniloramarojg@gmail.com>
2024-05-07 21:09:39 -0300

    teste2

---
+++ testes/teste2.py
@@ -0,0 +1,16 @@
+import os
+
+
+os.system('cls')
+
+# declaração
+a = 3
+resultado = ''
+
+# entrada de dados
+numero = int(input('digite um numero: '))
+
+resultado = a * numero
+
+#  saida
+print(f'o resultado de {a} * {numero} é {resultado}')
