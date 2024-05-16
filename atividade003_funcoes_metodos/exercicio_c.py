967eefbee1d609be9ddcd66be2e3eba176e0184b
daniloamarojf <daniloramarojg@gmail.com>
2024-05-15 21:27:28 -0300

    atividade003

--- atividade003_funcoes_metodos/exercicio_c.py
+++ atividade003_funcoes_metodos/exercicio_c.py
@@ -1,1 +1,17 @@
 # Faça um programa que receba as informações de base e expoente. Calcule a potência.
+# Importando bibliotecas
+
+import os
+import math
+
+
+print('-'*70)
+print('Para calculo da potência, digite a base e o expoente')
+print('-'*70)
+
+ 
+base = int(input('Digite a base: '))
+expoente = int(input('Digite e expoete: '))
+potencia = math.pow(base, expoente)
+print(f'A potência de {base} elevado a {expoente} é: {potencia}')
+print('Fim do programa')
--- atividade003_funcoes_metodos/exercicio_e.py
+++ atividade003_funcoes_metodos/exercicio_e.py
@@ -1,1 +1,14 @@
 # Faça um programa para sortear um número de 1 a 20.
+import os
+
+import random
+
+ 
+os.system('cls')
+
+print('-'*70)
+print('Sorteio de numero aleatorio de 1 a 20')
+print('-'*70)
+numero_aleatorio = random.randint(1, 20)
+print(f'O número sorteado é: {numero_aleatorio}')
+print('Fim do programa.')
