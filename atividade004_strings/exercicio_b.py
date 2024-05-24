de1689b44e8bf78223d722a346609abecb4e23ad
daniloamarojf <daniloramarojg@gmail.com>
2024-05-23 21:38:16 -0300

    atividade004_1

--- atividade004_strings/exercicio_b.py
+++ atividade004_strings/exercicio_b.py
@@ -1,2 +1,16 @@
 # Faça um programa que receba o nome 'João da Silva' e, 
 # em seguida, substitua 'Silva' por 'Oliveira'.
+
+
+import os
+
+
+os.system('cls')
+
+# declaração
+nome = 'João da Silva'
+novo_nome = nome.replace('Silva', 'Oliveira')
+
+# saida
+print(f'Nome: {nome}')
+print(f'Novo nome: {novo_nome}')
--- atividade004_strings/exercicio_c.py
+++ atividade004_strings/exercicio_c.py
@@ -1,2 +1,19 @@
 # Faça um programa que leia o nome de uma pessoa e verifique 
 # se a palavra 'Oliveira' está presente neste nome, mostrando True ou False.
+
+import os
+
+
+os.system('cls')
+
+# entrada de dados
+nome = str(input('Nome completo: '))
+respota1 = True
+reposta2 = False
+
+print()
+print(f'O nome "{nome}" contem "Oliveira"?')
+if 'Oliveira' in nome:
+    print(f'Resposta: {respota1} ')
+else:
+    print(f'Resposta: {reposta2}')
