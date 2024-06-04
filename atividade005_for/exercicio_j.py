f8471adbea182d81d86010d5dd2f33cd2990b2ea
daniloamarojf <daniloramarojg@gmail.com>
2024-06-03 21:30:48 -0300

    atividade005

---
+++ atividade005_for/exercicio_j.py
@@ -0,0 +1,3 @@
+# Crie um programa que realiza a contagem de 1 até 100, usando apenas de números ímpares, 
+# ao final do processo exiba na tela quantos números ímpares foram encontrados nesse intervalo, 
+# assim como a soma dos mesmos.
