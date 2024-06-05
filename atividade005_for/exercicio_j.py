3b5720b8dfcd4402737863e46414beefe4054cc1
daniloamarojf <daniloramarojg@gmail.com>
2024-06-04 21:33:27 -0300

    atividade005

--- atividade005_for/exercicio_j.py
+++ atividade005_for/exercicio_j.py
@@ -1,3 +1,23 @@
 # Crie um programa que realiza a contagem de 1 até 100, usando apenas de números ímpares, 
 # ao final do processo exiba na tela quantos números ímpares foram encontrados nesse intervalo, 
 # assim como a soma dos mesmos.
+
+import os
+
+
+os.system('cls')
+
+contador = 0
+soma = 0
+
+for var_iteradora in range(1, 100, 2):
+   
+    contador = contador + 1
+    soma += var_iteradora
+    
+print()
+print(f'O intervalo [1 a 100] possui {contador} números impar.')
+print(f'A soma dos números impares entre 1 e 100 é {soma}')
+    
+    
+    
--- atividade005_for/exercicio_k.py
+++ atividade005_for/exercicio_k.py
@@ -1,3 +1,21 @@
 # Crie um programa que pede que o usuário digite um nome ou uma frase,
 # verifique se esse conteúdo digitado é um palíndromo ou não,
 # exibindo em tela esse resultado.
+
+import os
+
+
+os.system('cls')
+
+frase = input('Frase: ')
+frase1 = frase.upper()
+frase_inverso = frase[::-1].upper()
+print()
+print(f'Frase original: {frase1}')
+print(f'Frase inversa: {frase_inverso}')
+print()
+
+if frase1 in frase_inverso:
+    print('Texto "é" um polídromo')
+else:
+    print('Texto "não" é polídromo')
