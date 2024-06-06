688b92080e8e46ad77534914b9f20595df63c5c8
daniloamarojf <daniloramarojg@gmail.com>
2024-06-05 21:33:10 -0300

    cadastro_pessoa_fisica

---
+++ gestao_de_clientes/gestao_de_clientes.py
@@ -0,0 +1,13 @@
+import os
+
+os.system('cls')
+
+# Cadastro de Cliente Pessoa Fisica
+nome = str(input('Nome:'))
+cpf = int(input('CPF: '))
+genero = str(input('Gênero: '))
+rua = str(input('Rua: '))
+numero_casa = str(input('Numero: '))
+complemeto_casa = int(input('Complemento: '))
+data_nascimento = int(input('Data de nascimento: '))
+email = str(input('E-mail:'))
