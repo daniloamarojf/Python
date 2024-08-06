80ee039888a2bd2041a073d14567a76bfa50aad2
daniloamarojf <daniloramarojg@gmail.com>
2024-08-05 21:32:17 -0300

    exercicios_função

--- atividade010_funcoes/exercicio_f.py
+++ atividade010_funcoes/exercicio_f.py
@@ -4,3 +4,21 @@
 #- Sua função deverá criar um dicionário contendo chave e valor utilizando 
 # como base essas duas listas. Depois, seu programa deverá imprimir esse 
 # dicionário utilizando uma estrutura de repetição for.
+
+import os
+
+
+os.system('cls')
+
+print('-'*50)
+print('Dicionários')
+print('50'*50)
+
+def criar_dicionario(chave, valor):
+    dicionario = {}
+    dicionario = zip(chave, valor)
+    return(dicionario)
+            
+lista1 = ['nome', 'peso', 'idade']
+lista2 = ['jhon', 40, 1.80]
+
